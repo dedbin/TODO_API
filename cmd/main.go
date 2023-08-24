53e224092b293319570c0d4d306e20fbfb329d6d
dedbin <black13dog@yandex.ru>
2023-08-24 18:44:49 +0300

    add config

--- cmd/main.go
+++ cmd/main.go
@@ -6,15 +6,26 @@ import (
 	service "TODO_API/pkg/service"
 	todo "TODO_API/smth"
 	"log"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("ошибка конфигурации: %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("ошибка сервера: %v", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
